587e801c70f1b48f83de74d745262d40d08f0ae1
mdegroot03 <max.de.groot.mdg@icloud.com>
2024-12-12 16:41:39 +0100

    Clean up for deployment

--- src/pages/MainPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import TestApiCallComponent from '../components/TestApiCallComponent.tsx';
-
-// For now main page contains only test component which makes API call
-const MainPage = () => <TestApiCallComponent />;
-
-export default MainPage;
