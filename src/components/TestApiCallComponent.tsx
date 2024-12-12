b812a3e156c54f07c1c7cc3a27bcc7ac34964255
mdegroot03 <max.de.groot.mdg@icloud.com>
2024-12-12 16:57:32 +0100

    fix build

--- src/components/TestApiCallComponent.tsx
+++ src/components/TestApiCallComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import getAndSetApiData from '../api/testApi';
+import getAndSetApiData from '../api/testApi.ts';
 
 // Test component which fetches data from an API and displays it
 const TestApiCallComponent = () => {
