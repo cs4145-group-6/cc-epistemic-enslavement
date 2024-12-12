b0a9d2680ed6a98d5d3e63b1839ed02163311486
Max de Groot <m.r.degroot-1@student.tudelft.nl>
2024-12-12 18:44:38 +0100

    fix path

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route index element={<MainPage />} />
-		<Route path="*/cc-epistemic-enslavement" element={<MainPage />} />
+		<Route path="*" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
   );
